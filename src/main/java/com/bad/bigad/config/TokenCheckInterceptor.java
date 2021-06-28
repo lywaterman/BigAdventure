28aa01673855e23aba6f92fa0a697fbd80a34c28
noname <ly@lydeMacBook-Pro.local>
2021-06-28 16:02:22 +0800

    ffff

---
+++ src/main/java/com/bad/bigad/config/TokenCheckInterceptor.java
@@ -0,0 +1,52 @@
+package com.bad.bigad.config;
+
+import com.bad.bigad.util.Util;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class TokenCheckInterceptor implements HandshakeInterceptor {
+    @Value("${jwt_key}")
+    String jwtKey;
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+        HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+        String token = httpServletRequest.getParameter("token");
+
+        Map<String, Object> claims = Util.instance.parseToken(token, jwtKey);
+
+        if(claims == null) {
+            log.info("websocket链接认证失败");
+            return false;
+        }
+        log.info("websocket链接认证成功");
+        String userName = (String) claims.get("wx_name");
+        //保存认证用户
+        attributes.put("user", new Principal() {
+            @Override
+            public String getName() {
+                return userName;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
--- src/main/java/com/bad/bigad/config/WebSocketConfig.java
+++ src/main/java/com/bad/bigad/config/WebSocketConfig.java
@@ -2,9 +2,15 @@ package com.bad.bigad.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -12,9 +18,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     HeaderParamInterceptor headerParamInterceptor;
 
+    @Autowired
+    TokenCheckInterceptor tokenCheckInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
+        registry.addEndpoint("/chat").addInterceptors(tokenCheckInterceptor)
+                .setHandshakeHandler(
+                        new DefaultHandshakeHandler() {
+                            @Override
+                            protected Principal determineUser(ServerHttpRequest request,
+                                                              WebSocketHandler wsHandler,
+                                                              Map<String, Object> attributes) {
+                                //设置认证用户
+                                return (Principal)attributes.get("user");
+                            }
+                        });
+
         registry.addEndpoint("/chat")
                 .setAllowedOrigins("*")
                 .withSockJS()
@@ -37,8 +57,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setSystemHeartbeatSendInterval(5000)
                 .setSystemHeartbeatReceiveInterval(4000);
     }
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(headerParamInterceptor);
-    }
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(headerParamInterceptor);
+//    }
 }
