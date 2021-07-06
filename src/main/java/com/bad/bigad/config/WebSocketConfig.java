5e495aa1db6190a7c293db10d70057c7541c9db1
lywaterman <liuyang124846@gmail.com>
2021-07-06 14:52:05 +0800

    加强聊天

--- src/main/java/com/bad/bigad/config/WebSocketConfig.java
+++ src/main/java/com/bad/bigad/config/WebSocketConfig.java
@@ -1,13 +1,16 @@
 package com.bad.bigad.config;
 
-import com.bad.bigad.component.ChatSocketHandler;
 import com.bad.bigad.component.GameSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -25,9 +28,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
 
     @Autowired
     GameSocketHandler gameSocketHandler;
-
-    @Autowired
-    ChatSocketHandler chatSocketHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -76,6 +76,26 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
                 .setSystemPasscode("guest")
                 .setSystemHeartbeatSendInterval(5000)
                 .setSystemHeartbeatReceiveInterval(4000);
+    }
+
+    //EndPoint有效
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.addDecoratorFactory(new WebSocketHandlerDecoratorFactory() {
+            @Override
+            public WebSocketHandler decorate(final WebSocketHandler handler) {
+                return new WebSocketHandlerDecorator(handler) {
+
+                    @Override
+                    public void afterConnectionEstablished(final WebSocketSession session) throws Exception {
+                        // We will store current user's session into WebsocketSessionHolder after connection is established
+                        String username = session.getPrincipal().getName();
+
+                        super.afterConnectionEstablished(session);
+                    }
+                };
+            }
+        });
     }
 
     @Bean
