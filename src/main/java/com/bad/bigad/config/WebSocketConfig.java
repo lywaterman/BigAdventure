f9e406650ab9e62aceb942956c070edf81e8338f
lywaterman <liuyang124846@gmail.com>
2021-07-08 00:08:28 +0800

    返回receipt

--- src/main/java/com/bad/bigad/config/WebSocketConfig.java
+++ src/main/java/com/bad/bigad/config/WebSocketConfig.java
@@ -6,11 +6,19 @@ import com.bad.bigad.manager.WsSessionManager;
 import com.bad.bigad.model.ChatMessage;
 import com.bad.bigad.service.ChatService;
 import com.bad.bigad.util.BridgeForJs;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ExecutorChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketHandler;
@@ -38,6 +46,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
 
     @Autowired
     ChatService chatService;
+
+    private MessageChannel outChannel;
+
+    @Autowired
+    public WebSocketConfig(MessageChannel clientOutboundChannel) {
+        this.outChannel = clientOutboundChannel;
+    }
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -137,8 +153,36 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(headerParamInterceptor);
+//    }
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(headerParamInterceptor);
+
+        registration.interceptors(new ExecutorChannelInterceptor() {
+
+            @Override
+            public void afterMessageHandled(Message<?> inMessage,
+                                            MessageChannel inChannel, MessageHandler handler, Exception ex) {
+
+                StompHeaderAccessor inAccessor = StompHeaderAccessor.wrap(inMessage);
+                String receipt = inAccessor.getReceipt();
+                if (StringUtils.isEmpty(receipt)) {
+                    return;
+                }
+
+                StompHeaderAccessor outAccessor = StompHeaderAccessor.create(StompCommand.RECEIPT);
+                outAccessor.setSessionId(inAccessor.getSessionId());
+                outAccessor.setReceiptId(receipt);
+                outAccessor.setLeaveMutable(true);
+
+                Message<byte[]> outMessage =
+                        MessageBuilder.createMessage(new byte[0], outAccessor.getMessageHeaders());
+
+                outChannel.send(outMessage);
+            }
+        });
     }
 }
