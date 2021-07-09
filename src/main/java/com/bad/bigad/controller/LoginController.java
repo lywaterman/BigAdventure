61c55fea08547662a77f2be2c8368b673e32980a
lywaterman <liuyang124846@gmail.com>
2021-07-09 16:30:08 +0800

    Update LoginController.java

--- src/main/java/com/bad/bigad/controller/LoginController.java
+++ src/main/java/com/bad/bigad/controller/LoginController.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
